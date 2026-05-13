bf6a73dadccf8ec1cc89cd4805f1f930d78a2061
ppaepke <pontus_1000@hotmail.com>
2026-05-13 11:13:19 +0200

    added conflict

--- LifeHacksThatMakesItWorseAPI/Program.cs
+++ LifeHacksThatMakesItWorseAPI/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(buil
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//Tittuut
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
